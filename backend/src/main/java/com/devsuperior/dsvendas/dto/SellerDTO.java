3f67b978f0c95553436e3b690a7b28ff5e8a7f71
Eric M.S <15831837+ericmss@users.noreply.github.com>
2021-05-05 13:58:04 -0300

    wip: set layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,43 @@
+package com.devsuperior.dsvendas.dto;
+
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.entities.Seller;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SellerDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String name;
+
+    public SellerDTO() {
+    }
+
+    public SellerDTO(Long id, String name, List<Sale> sales) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public SellerDTO(Seller entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
