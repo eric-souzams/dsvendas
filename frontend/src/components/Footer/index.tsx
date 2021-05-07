703cc8e3d29719b53d94a172585789db9cbb9a0a
Eric M.S <15831837+ericmss@users.noreply.github.com>
2021-05-07 15:28:37 -0300

    wip: fix Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,25 @@
+import './style.css';
+
+import { ReactComponent as YoutubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedinIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
 const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light"><small><strong>Semana Spring React</strong><br />
-       &copy; 2021 - DsVendas</small></p>
+          <strong className="text-light">&copy; 2021 - DS Vendas</strong>
+          <div className="footer-icons">
+            <a href="https://youtube.com/c/" target="_new">
+              <YoutubeIcon />
+            </a>
+            <a href="https://linkedin.com/" target="_new">
+              <LinkedinIcon />
+            </a>
+            <a href="https://instagram.com/" target="_new">
+              <InstagramIcon />
+            </a>
+          </div>
       </div>
     </footer>
   );
